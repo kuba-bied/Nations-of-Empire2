604708526c3e5a28bd3a753759f7711a3ce6a3d3
kuba-bied <pol-2004@o2.pl>
2023-03-11 11:31:30 +0100

    g

---
+++ sur.js
@@ -0,0 +1,40 @@
+console.log("sur.js wczytany")
+
+
+let clicks =0 ;
+let zloto =1000;
+let zelazo = 2500;
+let siladyplomacji =100;
+
+function add2(){
+    zloto++;
+    console.log(zloto)
+    document.getElementById("zl").innerHTML = 'Złoto: ' + zloto
+};
+
+
+function add3(){
+    clicks++;
+    console.log(clicks)
+    document.getElementById("we").innerHTML = 'Węgiel: ' + clicks
+};
+function add4(){
+    zelazo++;
+    zelazo++;
+    console.log(zelazo)
+    document.getElementById("ze").innerHTML = 'Żelazo: ' + zelazo
+};
+function add5(){
+    siladyplomacji++;
+    console.log(siladyplomacji)
+    document.getElementById("sd").innerHTML = 'SiłaDyplomacji: ' + siladyplomacji
+};
+
+
+
+function iframe(){
+    
+}
+
+var iframe 
+
